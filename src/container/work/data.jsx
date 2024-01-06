d11debde5209ff43b417a62c2086be409bc5a7b2
Rushikesh <50754786+RushiChavan-dev@users.noreply.github.com>
2024-01-05 20:35:29 -0500

    android project

--- src/container/work/data.jsx
+++ src/container/work/data.jsx
@@ -1,37 +1,195 @@
-import { images } from '../../constant/index'
+import { images } from "../../constant/index";
 
- const data = [
-    { title: "Javascript", content: [{title: "MERN Social Media", text: "This is a MERN stack Social Media Application", imgURL: images.Social_media, github: "https://github.com/RushiChavan-dev/MERN-ECOM", tag: "MERN stack"},
-                                  {title: "Ecommerce Website", text: "This a web application i have made using MongoDB, and React js", imgURL: images.about02, github: "https://github.com/RushiChavan-dev/ecommerence", tag: "Full stack "},
-                                  {title: "Simon Game", text: "This is a memory game called Simon, i have made it using JQuery.", imgURL: images.about03, github: "https://github.com/RushiChavan-dev/Simon_Game", live_site: "https://extraordinary-seahorse-a47368.netlify.app/", tag: "Front-end"},
-                                  {title: "ToDo List", text: "This a web application i have made using MongoDb and EJX templating", imgURL: images.about04, github: "https://github.com/", live_site:"https://javascriptbear.github.io/todo_react_app/", tag: "Back-end"},
-                                  {title: "Simple Calculator", text: "This a web application i have made using react.js", imgURL: images.about01, github: "https://github.com/RushiChavan-dev/Calculator", live_site: "https://codesandbox.io/p/github/RushiChavan-dev/Calculator/main", tag: "Front-end"}
-] },
-{ title: "Python", content: [{title: "Tiffin services", text: "This is a restful API for tiffin services using fastapi and aws", imgURL: images.about04, github: "https://github.com/RushiChavan-dev/fastapi-aws-lear", tag: "Restful Api"},
-                                  {title: "BlackJack_House", text: "This a game application coded in Python.", imgURL: images.about03, github: "https://github.com/RushiChavan-dev/Simple-Black-Jack", live_site: "https://replit.com/@RushiChavan-dev/Simple-Black-jack#main.py", tag: "Python"},
-                                  {title: "Snake Game", text: "This a Python application is based on famous Sanke and Food Game", imgURL: images.about02, github: "https://github.com/RushiChavan-dev/Sanke-in-Python", tag: "Game"},
-                                  
-] },
-{ title: "C++", content: [{title: "Clinic Magagement System", text: "This a C application, it is efficient appointment logic and patient data is stores in flat-file database", imgURL: images.about01, github: "https://github.com/RushiChavan-dev/Veterinary-Clinic-", tag: "C language"},
-                                  {title: "Library Management System", text: "This a C++ application which is using Object oriented logic in development and use flat-file database for storing Data.", imgURL: images.about02, github: "https://github.com/RushiChavan-dev/Library-mangment", tag: "C++ App"},
+const data = [
+  {
+    title: "Android ",
+    content: [
+      {
+        title: "Search App",
+        text: "This is a Search example in Compose UI",
+        imgURL: images.about03,
+        github: "https://github.com/RushiChavan-dev/AppSearchGuide",
+        tag: "Search Bar",
+      },
+      {
+        title: "Weather App ",
+        text: "Android Weather App ",
+        imgURL: images.about05,
+        github: "https://github.com/RushiChavan-dev/WeatherAPP",
+        tag: "Weather APP",
+      },
+      {
+        title: "Splash Screen",
+        text: "Example of Splash Screen",
+        imgURL: images.about03,
+        github: "https://github.com/RushiChavan-dev/AnimatedSplash",
+        tag: "Splash",
+      },
+      {
+        title: "Weather APP MVI",
+        text: "APP with MVI",
+        imgURL: images.about06,
+        github: "https://github.com/RushiChavan-dev/Weather-App-MVI",
+        tag: "MVI weather app",
+      },
+    ],
+  },
 
-                                  {title: "Mini C++ Projects", text: "This are the Mini C++ projects that i had made during 2nd Semester", imgURL: images.about03, github: "https://github.com/RushiChavan-dev/Basic-C-plus-plus-", tag: "C++ apps"},
-] },
-{ title: "Web App", content: [{title: "Personal Portfolio", text: "This a web page I have made using pure HTML, CSS, and Javascript", imgURL: images.about03, github: "", live_site: "https://github.com/", tag: "Portfolio"},
-                                  {title: "Web Application", text: "This is a web application which are develop while studying", imgURL: images.about01, github: "https://github.com/RushiChavan-dev/Web-application",},
-                                  
-] },
+  {
+    title: "Javascript",
+    content: [
+      {
+        title: "MERN Social Media",
+        text: "This is a MERN stack Social Media Application",
+        imgURL: images.Social_media,
+        github: "https://github.com/RushiChavan-dev/MERN-ECOM",
+        tag: "MERN stack",
+      },
+      {
+        title: "Ecommerce Website",
+        text: "This a web application i have made using MongoDB, and React js",
+        imgURL: images.about02,
+        github: "https://github.com/RushiChavan-dev/ecommerence",
+        tag: "Full stack ",
+      },
+      {
+        title: "Simon Game",
+        text: "This is a memory game called Simon, i have made it using JQuery.",
+        imgURL: images.about03,
+        github: "https://github.com/RushiChavan-dev/Simon_Game",
+        live_site: "https://extraordinary-seahorse-a47368.netlify.app/",
+        tag: "Front-end",
+      },
+      {
+        title: "ToDo List",
+        text: "This a web application i have made using MongoDb and EJX templating",
+        imgURL: images.about04,
+        github: "https://github.com/",
+        live_site: "https://javascriptbear.github.io/todo_react_app/",
+        tag: "Back-end",
+      },
+      {
+        title: "Simple Calculator",
+        text: "This a web application i have made using react.js",
+        imgURL: images.about01,
+        github: "https://github.com/RushiChavan-dev/Calculator",
+        live_site:
+          "https://codesandbox.io/p/github/RushiChavan-dev/Calculator/main",
+        tag: "Front-end",
+      },
+    ],
+  },
+  {
+    title: "Python",
+    content: [
+      {
+        title: "Tiffin services",
+        text: "This is a restful API for tiffin services using fastapi and aws",
+        imgURL: images.about04,
+        github: "https://github.com/RushiChavan-dev/fastapi-aws-lear",
+        tag: "Restful Api",
+      },
+      {
+        title: "BlackJack_House",
+        text: "This a game application coded in Python.",
+        imgURL: images.about03,
+        github: "https://github.com/RushiChavan-dev/Simple-Black-Jack",
+        live_site:
+          "https://replit.com/@RushiChavan-dev/Simple-Black-jack#main.py",
+        tag: "Python",
+      },
+      {
+        title: "Snake Game",
+        text: "This a Python application is based on famous Sanke and Food Game",
+        imgURL: images.about02,
+        github: "https://github.com/RushiChavan-dev/Sanke-in-Python",
+        tag: "Game",
+      },
+    ],
+  },
+  {
+    title: "C++",
+    content: [
+      {
+        title: "Clinic Magagement System",
+        text: "This a C application, it is efficient appointment logic and patient data is stores in flat-file database",
+        imgURL: images.about01,
+        github: "https://github.com/RushiChavan-dev/Veterinary-Clinic-",
+        tag: "C language",
+      },
+      {
+        title: "Library Management System",
+        text: "This a C++ application which is using Object oriented logic in development and use flat-file database for storing Data.",
+        imgURL: images.about02,
+        github: "https://github.com/RushiChavan-dev/Library-mangment",
+        tag: "C++ App",
+      },
 
-{ title: "Blockchain", content: [
-  
-  {title: "ERC 20 Token", text: "This is a smart contract based on ERC 20 token", imgURL: images.about03, github: "https://github.com/RushiChavan-dev/BCDV1010_Lab_9-Rushikesh-Chavan-ERC20", tag: "ERC 20 Tokens"},
-  {title: "Blockchain Broadcast Cardano", text: "Transform your books into tradable NFTs and unlock the potential to buy, sell, and resell in the digital marketplace", imgURL: images.about05, github: "https://github.com/RushiChavan-dev/Blockchain-Broadcast-Cardano", tag: "Blockchain Broadcast Cardano"},
-  {title: "Hyperledger Sample", text: "Hyperledger sample project", imgURL: images.about03, github: "https://github.com/RushiChavan-dev/HyperLedgerSampler", tag: "Hyperledger Sample"},
-  {title: "Hyperledger Forex", text: "Cross-platform payment using hyperledger", imgURL: images.about06, github: "https://github.com/RushiChavan-dev/Hyperledger-forex-crossplatform", tag: "Hyperledger Corss-Platform Payment"},
+      {
+        title: "Mini C++ Projects",
+        text: "This are the Mini C++ projects that i had made during 2nd Semester",
+        imgURL: images.about03,
+        github: "https://github.com/RushiChavan-dev/Basic-C-plus-plus-",
+        tag: "C++ apps",
+      },
+    ],
+  },
+  {
+    title: "Web App",
+    content: [
+      {
+        title: "Personal Portfolio",
+        text: "This a web page I have made using pure HTML, CSS, and Javascript",
+        imgURL: images.about03,
+        github: "",
+        live_site: "https://github.com/",
+        tag: "Portfolio",
+      },
+      {
+        title: "Web Application",
+        text: "This is a web application which are develop while studying",
+        imgURL: images.about01,
+        github: "https://github.com/RushiChavan-dev/Web-application",
+      },
+    ],
+  },
 
+  {
+    title: "Blockchain",
+    content: [
+      {
+        title: "ERC 20 Token",
+        text: "This is a smart contract based on ERC 20 token",
+        imgURL: images.about03,
+        github:
+          "https://github.com/RushiChavan-dev/BCDV1010_Lab_9-Rushikesh-Chavan-ERC20",
+        tag: "ERC 20 Tokens",
+      },
+      {
+        title: "Blockchain Broadcast Cardano",
+        text: "Transform your books into tradable NFTs and unlock the potential to buy, sell, and resell in the digital marketplace",
+        imgURL: images.about05,
+        github:
+          "https://github.com/RushiChavan-dev/Blockchain-Broadcast-Cardano",
+        tag: "Blockchain Broadcast Cardano",
+      },
+      {
+        title: "Hyperledger Sample",
+        text: "Hyperledger sample project",
+        imgURL: images.about03,
+        github: "https://github.com/RushiChavan-dev/HyperLedgerSampler",
+        tag: "Hyperledger Sample",
+      },
+      {
+        title: "Hyperledger Forex",
+        text: "Cross-platform payment using hyperledger",
+        imgURL: images.about06,
+        github:
+          "https://github.com/RushiChavan-dev/Hyperledger-forex-crossplatform",
+        tag: "Hyperledger Corss-Platform Payment",
+      },
+    ],
+  },
+];
 
-                                  
-] }
-  ];
-
-  export default data;
+export default data;
